60539c0f08e8b3a2031af7f9485c979b77f6618f
unknown <157027148@qq.com>
2020-11-01 00:52:29 +0800

    origin

---
+++ cmake-build-debug/CMakeFiles/1_20201101.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_20201101.exe"
+  "1_20201101.exe.manifest"
+  "1_20201101.pdb"
+  "CMakeFiles/1_20201101.dir/main.cpp.obj"
+  "lib1_20201101.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_20201101.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
